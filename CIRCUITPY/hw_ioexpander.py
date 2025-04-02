69a30696d41ae0afe492bbba12a837c38a6020ec
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-04-02 19:53:18 +0200

    Create hw_ioexpander.py

---
+++ CIRCUITPY/hw_ioexpander.py
@@ -0,0 +1,59 @@
+import board
+import busio
+
+# i2c adresses
+bus1 = 0x20
+bus2 = 0x21
+
+# Register addresses
+CONFIG_REG = 0x06  # Configuration register (input/output)
+OUTPUT_REG = 0x02  # Output register
+
+
+
+
+class PCA9535:
+    def __init__(self, i2c, address=bus1):
+        self.i2c = i2c
+        self.address = address
+        self.output_state = [0] * 16  # Default all outputs low
+        self._write_register(CONFIG_REG, 0x0000)  # Set all pins as output initially (output: 0x0000, input: 0xFFFF)
+
+    def _write_register(self, reg, value):
+        data = value.to_bytes(2, 'little')
+        while not self.i2c.try_lock():
+            pass
+        self.i2c.writeto(self.address, bytes([reg]) + data)
+        self.i2c.unlock()
+
+    def write_pin(self, pin, value):
+        pin += -1 # shift start from 1 to 0
+        if 0 <= pin < 16:
+            self.output_state[pin] = 1 if value else 0
+            output_value = sum([(1 << i) if self.output_state[i] else 0 for i in range(16)])
+            self._write_register(OUTPUT_REG, output_value)
+        else:
+            raise Exception("Pin out of range!")
+
+
+
+
+def set_pin(bus, pin, value):
+    """
+    Set pin high (True) or low (False)
+    There is bus 1 and bus 2
+    Pins range from 1 to 16
+    """
+    if bus == 1:
+        bus = b1
+    else:
+        bus = b2
+    bus.write_pin(pin, value)
+
+
+
+
+i2c = busio.I2C(scl=board.GP3, sda=board.GP2)
+
+b1 = PCA9535(i2c, bus1)
+b2 = PCA9535(i2c, bus2)
