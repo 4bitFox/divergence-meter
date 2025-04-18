d0400f0e5db947606569fcbd0b36c0552eda1362
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-04-18 22:00:40 +0200

    Update main.py

--- CIRCUITPY/main.py
+++ CIRCUITPY/main.py
@@ -1,5 +1,8 @@
 import hw_rtc as rtc
+import hw_nixie as n
 import dt
+import an_test as test
+import an_divergence as divergence
 
 
 struct_time = rtc.get_struct_time()
@@ -8,36 +11,12 @@ print(d)
 
 
 
-import hw_nixie as n
-from time import sleep
-
-def test(t = 1):
-    dot_pos = 1
-    while True:
-        for digit in range(0, 11):
-            if digit == 10:
-                for tube in range(1, 9):
-                    n.set_digit(None, tube)
-            else:
-                for tube in range(1, 9):
-                    print(digit, tube)
-                    n.set_digit(digit, tube)
-            
-            n.set_dot(dot_pos, "R")
-            n.set_dot(dot_pos, "L")
-            if dot_pos == None:
-                dot_pos = 1
-            else:
-                dot_pos += 1
-            if dot_pos > 8:
-                dot_pos = None
-            
-            n.update()
-            sleep(t)
-
 
 try:
-    test()
+    test.animation(0.5)
+    while True:
+        divergence.animation()
 except BaseException as e:
+    print("Error:", e)
+finally:
     n.all_off()
-    print(e)
