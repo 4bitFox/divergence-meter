2c228fef9ebf42142a3bcb9c8c6c81310e65dfb6
4bitFox <33175205+4bitFox@users.noreply.github.com>
2025-04-25 19:57:18 +0200

    Update main.py

--- CIRCUITPY/main.py
+++ CIRCUITPY/main.py
@@ -3,20 +3,26 @@ import an_test
 import an_divergence
 import an_dt
 import dt
+from time import sleep
 
 
-
+#dt.set_dt(2024, 01, 01, 03, 58, 50)
 
 try:
     an_test.animation(0.5)
+    sleep(1)
     an_divergence.animation()
+    sleep(1)
+    an_dt.ntp_sync(force=True)
     while True:
         an_dt.time()
         an_divergence.animation()
         an_dt.date()
         an_dt.misc()
+        
+        # routines
         an_test.routine()
-        dt.sync_ntp_routine()
+        an_dt.ntp_sync()
 except BaseException as e:
     print("CRITICAL ERROR:", e)
 finally:
